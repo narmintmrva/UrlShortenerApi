d75b61a2a13fec090d5f439a49cfc08bc59147a7
Narmin Tomarova <tmrva621@gmail.com>
2025-07-19 00:02:26 +0400

    Database context

---
+++ UrlShortenerApi/UrlShortenerApi/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortenerApi.Models;
+
+namespace UrlShortenerApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+    }
+}
--- UrlShortenerApi/UrlShortenerApi/Program.cs
+++ UrlShortenerApi/UrlShortenerApi/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using UrlShortenerApi.Data;
+
 namespace UrlShortenerApi
 {
     public class Program
@@ -8,8 +11,10 @@ namespace UrlShortenerApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
